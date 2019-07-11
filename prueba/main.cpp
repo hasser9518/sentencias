3b65264c4344729ab59150df14c095433543bccf
Hasser Perez <hasser.perez9518@est.uca.edu.ni>
2019-07-11 14:38:00 -0600

    Initial Commit

---
+++ prueba/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prueba
+//
+//  Created by Hasser Ismael Perez Rizo on 7/11/19.
+//  Copyright © 2019 Hasser Ismael Perez Rizo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
